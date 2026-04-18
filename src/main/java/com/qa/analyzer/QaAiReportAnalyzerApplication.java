d34a4b5b221cd0aed7f0ef6d2c4c0599b7fae7c4
Ravi170210 <133086980+Ravi170210@users.noreply.github.com>
2026-04-18 13:20:45 +0530

    Create Spring Boot main class

---
+++ src/main/java/com/qa/analyzer/QaAiReportAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.qa.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaAiReportAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaAiReportAnalyzerApplication.class, args);
+    }
+}
